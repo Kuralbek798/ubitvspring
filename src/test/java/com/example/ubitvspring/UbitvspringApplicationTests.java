d808e158aec0c296d131a7f4d6128ac665784756
Kuralbek798 <agubaev_kuralbek@mail.ru>
2022-03-24 00:19:31 +0600

    24032022

---
+++ src/test/java/com/example/ubitvspring/UbitvspringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ubitvspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UbitvspringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
